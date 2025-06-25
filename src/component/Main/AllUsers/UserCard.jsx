1d26cb9f72f4b22f3bfe5b895d97706e0eec008e
Md Rakib Ali <rakib2020.tkg@gmail.com>
2025-06-25 16:32:39 +0600

    feat: Add modal functionality to UserCard and ProfileVerificationCard for user actions

--- src/component/Main/AllUsers/UserCard.jsx
+++ src/component/Main/AllUsers/UserCard.jsx
@@ -1,10 +1,20 @@
+/* eslint-disable react/prop-types */
 import moment from "moment";
 import { imageBaseUrl } from "../../../config/imageBaseUrl";
+import { useState } from "react";
+import { Modal } from "antd";
 
 // eslint-disable-next-line react/prop-types
 const UserCard = ({ user }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [openModalType, setOpenModalType] = useState("block");
   // eslint-disable-next-line react/prop-types
   const { fullName, email, profileImage, createdAt, phoneNumber } = user;
+
+  const handleOpenModal = (type) => {
+    setOpenModalType(type);
+    setIsModalOpen(true);
+  };
 
   return (
     <div className="w-full p-4 border border-[#777777] bg-white rounded-lg space-y-5">
@@ -39,13 +49,45 @@ const UserCard = ({ user }) => {
         </div>
       </div>
       <div className="flex justify-end items-center gap-5">
-        <button className="px-5 py-2 border border-rose-500 text-rose-500 rounded-lg">
+        <button
+          onClick={() => handleOpenModal("suspend")}
+          className="px-5 py-2 border border-rose-500 text-rose-500 rounded-lg"
+        >
           Suspend
         </button>
-        <button className="px-5 py-2 border border-rose-500 text-rose-500 rounded-lg">
+        <button
+          onClick={() => handleOpenModal("block")}
+          className="px-5 py-2 border border-rose-500 text-rose-500 rounded-lg"
+        >
           Block
         </button>
       </div>
+      <Modal
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={null}
+        centered
+        title={openModalType === "block" ? "Block User" : "Suspend User"}
+      >
+        <div className="w-full border p-2">
+          <img
+            src={`${imageBaseUrl}${profileImage?.imageUrl}`}
+            className="w-full h-56"
+            alt="user"
+          />
+          <div className="space-y-2 py-5 font-semibold text-gray-800">
+            <h1>User Name : {fullName}</h1>
+            {/* user email */}
+            <h1>Email : {email}</h1>
+            {/* user phone number */}
+            <h1>Joining Date : {moment(createdAt).format("DD-MM-YYYY")}</h1>
+          </div>
+        </div>
+        <div className="flex justify-center mt-8 items-center gap-5">
+          <button onClick={() => setIsModalOpen(false)} className="px-5 py-2 border border-gray-500 text-gray-500 rounded-md">Go Back</button>
+          <button className="px-8 py-2 border border-rose-500 text-rose-500 rounded-md">{openModalType === "block" ? "Block" : "Suspend"}</button>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/component/Main/ProfileVerifications/ProfileVerificationCard.jsx
+++ src/component/Main/ProfileVerifications/ProfileVerificationCard.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/prop-types */
 import moment from "moment";
 import { imageBaseUrl } from "../../../config/imageBaseUrl";
+import { useState } from "react";
+import { Modal } from "antd";
 
 // eslint-disable-next-line react/prop-types
 const ProfileVerificationCard = ({ user }) => {
   // eslint-disable-next-line react/prop-types
   const { fullName, email, profileImage, createdAt } = user;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
   return (
     <div className="w-full p-4 border border-[#777777] bg-white rounded-lg space-y-5">
@@ -38,10 +45,37 @@ const ProfileVerificationCard = ({ user }) => {
         <button className="px-5 py-2 bg-blue-500 text-white rounded-lg">
           Verify
         </button>
-        <button className="px-5 py-2  bg-primary rounded-lg">
+        <button
+          onClick={handleOpenModal}
+          className="px-5 py-2  bg-primary rounded-lg"
+        >
           View Media
         </button>
       </div>
+
+      <Modal
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={null}
+        title="Media"
+      >
+        <img
+          src={`${imageBaseUrl}${profileImage?.imageUrl}`}
+          alt={fullName}
+          className="w-full h-96 object-cover"
+        />
+        <div className="flex justify-center mt-5 items-center gap-5">
+          <button
+            onClick={() => setIsModalOpen(false)}
+            className="px-5 py-2 border border-gray-800 text-gray-800 rounded-lg"
+          >
+            Go Back
+          </button>
+          <button className="px-8 py-2 bg-blue-500 text-white rounded-lg">
+            Verify
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
